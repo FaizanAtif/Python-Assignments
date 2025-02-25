7a59c1a8139c57b508ee04bb8012d8975ef4274e
FaizanAtif <FAIZANATIF183@GMAIL.COM>
2025-02-26 00:12:37 +0500

    first commit

---
+++ datatypes and special keywords/main.py
@@ -0,0 +1,40 @@
+# First Class Hello World
+print("Hello, World!")
+
+# -------------------------------
+
+# Pycache Example
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Asharib"))
+
+# -------------------------------
+
+# data types
+
+# string
+name = "Asharib"
+
+# integer
+age = 21
+
+# float
+height = 5.7
+
+# boolean
+is_student = True
+
+# list
+grades = [95, 83, 90, 87, 74]
+
+# tuple
+coordinates = (3, 5)
+
+# dictionary
+person = {"name": "Asharib", "age": 21, "height": 5.7}
+
+# set
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
